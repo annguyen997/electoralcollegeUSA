2480ca1752267d97536e5541f4db795b3ee2aeac
An Nguyen <32471775+annguyen997@users.noreply.github.com>
2018-06-04 22:28:35 -0400

    Update Electoral.java

--- Electoral.java
+++ Electoral.java
@@ -1,4 +1,4 @@
-import com.sun.istack.internal.NotNull;
+//import com.sun.istack.internal.NotNull;
 
 import java.io.*;
 import java.lang.reflect.Array;
@@ -10,12 +10,13 @@ public class Electoral {
 
     protected static final int ELECTORALWIN = 270;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //Create Scanner object to read console input.
         Scanner in = new Scanner(System.in);
 
         //Get the list of all states and their associated electoral votes.
-        ArrayList<States> electoralMap = new ArrayList<States>(CreateStates());
+        ArrayList<States> electoralMap = new ArrayList<States>();
+        CreateStates(electoralMap);
 
         //Create presidential candidates.
         String[] candidateInfo = createCandidate(in, 1);
@@ -28,28 +29,34 @@ public class Electoral {
         printResults(candidate1, candidate2);
     }
 
-    public static ArrayList<States> CreateStates(){
+    public static void CreateStates(ArrayList<States> electoralMap) throws FileNotFoundException{
         /* Collect all U.S. states and their electoral votes into an ArrayList from files */
-        ArrayList<States> electoralMap = new ArrayList<States>();
 
         try {
+            /* Open the files of states and electoral votes for program */
+            //File fileStates = new File("States.txt");
             File fileStates = new File("src/States.txt");
             Scanner in = new Scanner(fileStates);
 
+            //File fileNumbers = new File("ElectoralVotes.txt");
             File fileNumbers = new File("src/ElectoralVotes.txt");
             Scanner in2 = new Scanner(fileNumbers);
 
-            while (in.hasNextLine()){
+            //File fileStatesAbbreviation = new File("StatesAbbreviation.txt");
+            File fileStatesAbbreviation = new File("src/StatesAbbreviation.txt");
+            Scanner in3 = new Scanner(fileStatesAbbreviation);
+
+            while (in.hasNextLine() && in3.hasNextLine()){
                 String stateName = in.nextLine();
                 int electoralVotes = in2.nextInt();
-                electoralMap.add(new States(stateName, electoralVotes));
+                String stateAbbreviation = in3.nextLine();
+                electoralMap.add(new States(stateName, electoralVotes, stateAbbreviation));
             }
 
         } catch (FileNotFoundException e){
             System.out.println("File Not Found");
             e.printStackTrace();
         }
-        return electoralMap;
     }
 
     public static String[] createCandidate(Scanner in, int number){
@@ -65,22 +72,24 @@ public class Electoral {
     }
 
     public static void electionResults(ArrayList<States> electoralMap, Scanner in,
-                                Candidate candidate1, Candidate candidate2){
+                                       Candidate candidate1, Candidate candidate2){
         /* Assign states and electoral votes to candidates*/
 
         Random random = new Random(); //For testing purposes
 
-        for (int i = 0; i < electoralMap.size(); i++){
+        //for (int i = 0; i < electoralMap.size(); i++){
+        for (States state: electoralMap){
+
             //For each U.S. state and its electoral votes.
-            String stateName = electoralMap.get(i).getStateName();
-            int electoralVotes = electoralMap.get(i).getElectoral_Votes();
+            String stateName = state.getStateAbbreviation();
+            int electoralVotes = state.getElectoral_Votes();
 
             //Randomly assign states (for testing purposes)
             int value = random.nextInt(2);
             if (value == 0){
-                candidate1.stateWon(stateName, electoralVotes);
+                candidate1.stateWon(state, stateName, electoralVotes);
             } else {
-                candidate2.stateWon(stateName, electoralVotes);
+                candidate2.stateWon(state, stateName, electoralVotes);
             }
 
             /*
